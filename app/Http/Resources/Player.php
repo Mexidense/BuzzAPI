dbd7f2bd70579df4084e21783bfa0c876c33fe23
Salvador B <salvador.briones@selectra.info>
2019-04-29 14:40:05 +0200

    Player controllers developed

--- app/Http/Resources/Player.php
+++ app/Http/Resources/Player.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Player extends JsonResource
@@ -9,10 +10,11 @@ class Player extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
-    public function toArray($request) {
+    public function toArray($request)
+    {
         return [
             'id'         => $this->id,
             'name'       => $this->name,
